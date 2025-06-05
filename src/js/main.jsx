fa4575fb8e069cd93bd0d2cbb7cb15f8bdbb322c
aiadburo <aiadburo@hotmail.com>
2025-06-05 19:33:40 +0000

    todolist-fetch

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -5,11 +5,14 @@ import ReactDOM from 'react-dom/client'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap"
 
+//Font Awesome
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 // index.css'
 import '../styles/index.css'
 
 // components
-import Home from './components/Home';
+import Home from './components/Home.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
